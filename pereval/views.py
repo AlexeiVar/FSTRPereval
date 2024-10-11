ef9f7b883bdfba7af957b4da838e8bd9afafab57
ForTheJerusalem <kobheg81@gmail.com>
2024-10-11 19:49:41 +0300

    Небольшое изменение POST
    Немного изменен viewset, отправляет данные по заданию коды.

--- pereval/views.py
+++ pereval/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import status
 
 from .serializers import *
 from .models import *
@@ -9,3 +10,10 @@ from .models import *
 class PerevalViewset(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('Отправлено успешно', status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
