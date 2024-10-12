da91e6fc8dd78854066f6903156aaa87120b7501
ForTheJerusalem <kobheg81@gmail.com>
2024-10-12 15:31:49 +0300

    Проверка изменения данных пользователя
    Проверяю были ли изменены данные пользователя, если да то кидаю ошибку.

--- pereval/views.py
+++ pereval/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import status
+from django.forms.models import model_to_dict
 
 from .serializers import *
 from .models import *
@@ -17,3 +18,15 @@ class submitData(viewsets.ModelViewSet):
             serializer.save()
             return Response('Отправлено успешно', status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        data = request.data
+        user_dict = model_to_dict(instance.user)
+        # В дате нету айди юзера, поэтому удаляю его из нашего словаря
+        del user_dict['id']
+        if not data['user'] == user_dict:
+            response = {'state': 0, 'message': 'данные пользователя нельзя переопределить'}
+            return Response(response)
+        response = {'state': 1}
+        return Response(response)
